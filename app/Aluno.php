ee82142b621af84cc9561b9f89df450a0c06326b
isaiassm <isaias1113@outlook.com>
2019-02-07 16:36:39 -0200

    [ADD] Adicionando mass assigned fillable nas models

--- app/Aluno.php
+++ app/Aluno.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Aluno extends Model
 {
-    //
+    protected $fillable = ['name', 'idade', 'telefone', 'cpf', 'cep', 'sexo', 'created_at', 'updated_at'];
 }
--- app/Professor.php
+++ app/Professor.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 
 class Professor extends Model
 {
-    //
+    protected $fillable = ['name', 'idade', 'materia', 'telefone', 'cpf', 'cep', 'sexo', 'created_at', 'updated_at'];
 }
