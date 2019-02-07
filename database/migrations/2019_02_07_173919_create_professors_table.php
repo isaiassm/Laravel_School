1b76336db7a3c7ab24c79b71ea5e1dbc0bbe3f76
isaiassm <isaias1113@outlook.com>
2019-02-07 16:14:16 -0200

    [ADD] Criação de models e migrations com tabelas montadas sem relação

---
+++ database/migrations/2019_02_07_173919_create_professors_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfessorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('professors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 100);
+            $table->integer('idade');
+            $table->string('materia');
+            $table->integer('telefone');
+            $table->integer('cpf');
+            $table->integer('cep');
+            $table->string('sexo', 2);
+            
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('professors');
+    }
+}
